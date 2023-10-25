58026d557b87bd86c0e8f223efc5b5ded07d3345
Jason Hernandez <jasonh777y@gmail.com>
2023-10-25 12:33:28 -0700

    Added a navigation drawer to the home page

--- app/src/main/java/com/example/mylogin/HomePage.java
+++ app/src/main/java/com/example/mylogin/HomePage.java
@@ -1,7 +1,11 @@
 package com.example.mylogin;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,14 +16,19 @@ import android.view.MenuItem;
 import com.example.mylogin.databinding.ActivityHomePageBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class HomePage extends AppCompatActivity {
+    DrawerLayout DrawerLayout;
+    Toolbar toolbar;
+    FragmentManager fragmentManager;
     BottomNavigationView bottomNavigationView;
     HomeFragment homeFragment= new HomeFragment();
     SettingsFragment settingsFragment = new SettingsFragment();
     ProfileFragment profileFragment = new ProfileFragment();
+    MessageFragment messageFragment = new MessageFragment();
 
 
 
@@ -28,34 +37,70 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_home_page);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, DrawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        DrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setBackground(null);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
-                            return true;
-                    case R.id.profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
-                        return true;
-
-                    case R.id.settings:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
-                        return true;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId==R.id.home) {
+                    openFragment(new HomeFragment());
+                    return true;
+                } else if  (itemId==R.id.settings){
+                    openFragment(new SettingsFragment());
+                    return true;
+                }else if  (itemId==R.id.profile) {
+                    openFragment(new ProfileFragment());
+                    return true;
                 }
 
                 return false;
             }
         });
+
+        fragmentManager = getSupportFragmentManager();
+        openFragment(new HomeFragment());
+
+
+
+
         
 
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId==R.id.nav_message){
+            openFragment(new MessageFragment());
+        }
+
+        DrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed(){
+        if (DrawerLayout.isDrawerOpen(GravityCompat.START)){
+            DrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+    private void openFragment(Fragment fragment){
+        FragmentTransaction transaction =fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.commit();
+    }
 }
--- app/src/main/java/com/example/mylogin/MainActivity.java
+++ app/src/main/java/com/example/mylogin/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mylogin;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
