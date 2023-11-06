75202a23acefdc24ba0fc6ae9bd85600e137b4bd
Jason Hernandez <jasonh777y@gmail.com>
2023-11-06 10:43:00 -0800

    Added a toggle for hiding and showing password on the registration page

--- app/src/main/java/com/example/mylogin/Registration.java
+++ app/src/main/java/com/example/mylogin/Registration.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,6 +17,8 @@ import org.w3c.dom.Text;
 public class Registration extends AppCompatActivity {
     public TextView RegisterButton;
     public TextView AlreadyHaveAccountButton;
+    EditText password;
+    boolean passwordVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,42 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         RegisterButton = (Button) findViewById(R.id.reg_btn);
         AlreadyHaveAccountButton = (TextView) findViewById(R.id.already_have_account_link);
+
+        password = findViewById(R.id.register_password);
+
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int Right=2;
+                if(motionEvent.getAction()==MotionEvent.ACTION_UP)
+                {
+                    if (motionEvent.getRawX()>=password.getRight()-password.getCompoundDrawables()[Right].getBounds().width())
+                    {
+                        int selection=password.getSelectionEnd();
+                        if(passwordVisible)
+                        {
+                            //set drawable
+                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.baseline_visibility_off_24,0);
+                            //for hide password
+                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible=false;
+                        }else
+                        {
+                            //set drawable
+                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.password_icon,0);
+                            //for show password
+                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible=true;
+
+
+                        }
+                        password.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
 
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
